7a911b9a99e7f18cb24470ff0fbdd056cb2a5d73
wastedsoul123 <81835975+wastedsoul123@users.noreply.github.com>
2021-04-03 12:07:22 +0800

    Add files via upload

---
+++ InterGalacticAirways/InterGalacticAirways/DataProvider/StarShipDataProvider.cs
@@ -0,0 +1,35 @@
+﻿using InterGalacticAirways.Entity;
+using InterGalacticAirways.Interface;
+using InterGalacticAirways.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterGalacticAirways.DataProvider
+{
+    public class StarShipDataProvider: BaseDataProvider
+    {
+
+        public List<StarShip> GetAll()
+        {
+
+            IRepository<StarShipList> starshipRepo = new Repository<StarShipList>(client);
+            List<StarShip> returnedObj = new List<StarShip>();
+            string next = null;
+
+            do
+            {
+                var ship = starshipRepo.GetByUrl(next);
+                returnedObj.AddRange(ship.Result.results);
+                next = ship.Result.next;
+
+            } while (!string.IsNullOrEmpty(next));
+
+            return returnedObj;
+
+        }
+
+    }
+}
