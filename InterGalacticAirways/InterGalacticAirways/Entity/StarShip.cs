6429d42cf37cabc386183c88dab5e0e48ed9314b
wastedsoul123 <81835975+wastedsoul123@users.noreply.github.com>
2021-04-03 11:34:24 +0800

    Add files via upload

---
+++ InterGalacticAirways/InterGalacticAirways/Entity/StarShip.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterGalacticAirways.Entity
+{
+    public class StarShip 
+    {
+
+
+        [JsonProperty]
+        public string name { get; set; }
+
+        [JsonProperty(PropertyName = "passengers")]
+        public string passengersRaw { get; set; }
+
+        public List<string> pilots { get; set; }
+
+        public int Passengers
+        {
+            get
+            {
+                int n;
+                return int.TryParse(passengersRaw, out n) ? n : 0;
+            }
+        }
+
+    }
+    public class StarShipList : BaseEntity
+    {
+        private const string UrlToEntity = "starships";
+        protected override string Path
+        {
+            get
+            {
+                return UrlToEntity;
+
+            }
+
+        }
+        [JsonProperty]
+        public string next { get; set; }
+
+        [JsonProperty]
+        public List<StarShip> results { get; set; }
+
+
+    }
+}
---
+++ InterGalacticAirways/InterGalacticAirways/Helper/EntityInstance.cs
@@ -0,0 +1,18 @@
+﻿using InterGalacticAirways.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterGalacticAirways.Helper
+{
+
+
+    public static class EntityInstance<T> where T : BaseEntity
+    {
+        public static readonly Func<T> Instance = Expression.Lambda<Func<T>>(Expression.New(typeof(T))).Compile();
+    }
+
+}
